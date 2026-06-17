44f625fb3fddfae692e3e6758a6c7720a56f2c26
Abhishek Yadav <abhishekyadav6502@gmail.com>
2026-06-17 22:26:40 +0530

    Created project skeleton along with the ENUMS required

---
+++ src/main/java/com/desirArman/tickets/domain/EventStatusEnum.java
@@ -0,0 +1,5 @@
+package com.desirArman.tickets.domain;
+
+public enum EventStatusEnum {
+    DRAFT, PUBLISHED, CANCELLED, COMPLETED
+}
---
+++ src/main/java/com/desirArman/tickets/domain/TicketStatusEnum.java
@@ -0,0 +1,5 @@
+package com.desirArman.tickets.domain;
+
+public enum TicketStatusEnum {
+    PURCHASED , CANCELLED
+}
